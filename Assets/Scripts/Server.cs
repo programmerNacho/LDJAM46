236cce6ab9341a26122c537d62653bd938fad1e5
Ignacio <ignacio.programmer@gmail.com>
2020-04-19 02:55:45 +0200

    Server Prototype.

---
+++ Assets/Scripts/Server.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Server : MonoBehaviour
+{
+    [SerializeField]
+    private int initialVisualizationPerSecond;
+    [SerializeField]
+    private float maxTemperature;
+    [SerializeField]
+    private float minTemperature;
+    [SerializeField]
+    private int maxVisualizationPerSecond;
+    [SerializeField]
+    private float maxTemperatureRaiseVelocity;
+    [SerializeField]
+    private float maxTemperatureDecreaseVelocity;
+    [SerializeField]
+    public PornCategory pornCategory;
+
+    private int currentTotalVisualizationInt;
+    public int CurrentTotalVisualizationInt
+    {
+        get
+        {
+            return currentTotalVisualizationInt;
+        }
+        private set
+        {
+            currentTotalVisualizationInt = value;
+        }
+    }
+
+    private float currentTotalVisualizationFloat;
+    public float CurrentTotalVisualizationFloat
+    {
+        get
+        {
+            return currentTotalVisualizationFloat;
+        }
+        private set
+        {
+            currentTotalVisualizationFloat = value;
+        }
+    }
+
+    private float currentTemperature;
+    public float CurrentTemperature
+    {
+        get
+        {
+            return currentTemperature;
+        }
+        private set
+        {
+            currentTemperature = value;
+        }
+    }
+
+    private int currentVisualizationPerSecond;
+    public int CurrentVisualizationPerSecond
+    {
+        get
+        {
+            return currentVisualizationPerSecond;
+        }
+        private set
+        {
+            currentVisualizationPerSecond = value;
+        }
+    }
+
+    private float currentTemperatureChangeVelocity;
+    public float CurrentTemperatureChangeVelocity
+    {
+        get
+        {
+            return currentTemperatureChangeVelocity;
+        }
+        private set
+        {
+            currentTemperatureChangeVelocity = value;
+        }
+    }
+
+    private const float secondsBlocked = 10f;
+
+    private void Start()
+    {
+        currentVisualizationPerSecond = initialVisualizationPerSecond;
+    }
+
+    private bool blocked;
+
+    private void Update()
+    {
+        if(!blocked)
+        {
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                currentVisualizationPerSecond += 200;
+            }
+            if (currentTemperature >= maxTemperature)
+            {
+                StartCoroutine(BlockState());
+            }
+        }
+
+        currentTemperatureChangeVelocity = Mathf.Lerp(maxTemperatureDecreaseVelocity, maxTemperatureRaiseVelocity, (float)currentVisualizationPerSecond / maxVisualizationPerSecond);
+        currentTemperature = Mathf.Clamp(currentTemperature + currentTemperatureChangeVelocity * Time.deltaTime, minTemperature, maxTemperature);
+        currentVisualizationPerSecond = Mathf.Clamp(currentVisualizationPerSecond, 0, maxVisualizationPerSecond);
+        currentTotalVisualizationFloat += currentVisualizationPerSecond * Time.deltaTime;
+        currentTotalVisualizationInt = Mathf.CeilToInt(currentTotalVisualizationFloat);
+    }
+
+    private IEnumerator BlockState()
+    {
+        blocked = true;
+        currentVisualizationPerSecond = 0;
+        yield return new WaitForSeconds(secondsBlocked);
+        blocked = false;
+    }
+}
---
+++ Assets/Scripts/ServerStateCanvas.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ServerStateCanvas : MonoBehaviour
+{
+    [SerializeField]
+    private TextMeshProUGUI categoryText;
+    [SerializeField]
+    private TextMeshProUGUI temperatureText;
+    [SerializeField]
+    private TextMeshProUGUI changeTemperatureText;
+    [SerializeField]
+    private TextMeshProUGUI visualizationsText;
+    [SerializeField]
+    private TextMeshProUGUI totalVisualizationsText;
+
+    private Server server;
+
+    private void Start()
+    {
+        server = GetComponent<Server>();
+        categoryText.text = "Category: " + server.pornCategory;
+    }
+
+    private void Update()
+    {
+        temperatureText.text = "Temperature: " + server.CurrentTemperature.ToString("F2");
+        changeTemperatureText.text = "Chan. Vel. Temp: " + server.CurrentTemperatureChangeVelocity.ToString("F2");
+        visualizationsText.text = "Visualizations Per Second: " + server.CurrentVisualizationPerSecond;
+        totalVisualizationsText.text = "Total Visualizations: " + server.CurrentTotalVisualizationInt;
+    }
+}
