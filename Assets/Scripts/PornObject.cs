c95333f3e1d5119961b1b98ccc42b9a5050bcce7
Ignacio <ignacio.programmer@gmail.com>
2020-04-19 20:16:33 +0200

    Objects prefabs for Rooms.

---
+++ Assets/Scripts/PornObject.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LDJAM46
+{
+    [System.Serializable]
+    public class PornObjectStats
+    {
+        public PornCategory category;
+        public float percentageBonus;
+    }
+
+    [System.Serializable]
+    public class PornObjectInfo
+    {
+        public List<PornObjectStats> categoryPercentages = new List<PornObjectStats>();
+    }
+
+    public class PornObject : MonoBehaviour
+    {
+        public PornObjectInfo pornObjectInfo;
+    }
+}
---
+++ Assets/Scripts/RoomResultsManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LDJAM46
+{
+    public class RoomResultsManager : MonoBehaviour
+    {
+        [SerializeField]
+        private PornCategory roomType;
+        [SerializeField]
+        private float visualizationsPerSecondBase;
+
+        private List<PornObjectInfo> pornObjectsInfo;
+
+        private void Start()
+        {
+            pornObjectsInfo = new List<PornObjectInfo>();
+        }
+
+        public void AddPornObjectInfo(PornObjectInfo pornObjectInfo)
+        {
+            pornObjectsInfo.Add(pornObjectInfo);
+        }
+
+        public void CalculateResults()
+        {
+            float percentageBonus = 0f;
+            for (int i = 0; i < pornObjectsInfo.Count; i++)
+            {
+                for (int j = 0; j < pornObjectsInfo[i].categoryPercentages.Count; j++)
+                {
+                    if (pornObjectsInfo[i].categoryPercentages[j].category == roomType)
+                    {
+                        percentageBonus += pornObjectsInfo[i].categoryPercentages[j].percentageBonus;
+                    }
+                }
+            }
+
+            pornObjectsInfo.Clear();
+
+            float bonus = visualizationsPerSecondBase * percentageBonus;
+            float result = visualizationsPerSecondBase + bonus;
+            Debug.Log(result);
+        }
+    }
+}
