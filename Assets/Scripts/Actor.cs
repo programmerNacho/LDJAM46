09e54247a5533989ed36c1756561047f3775f3f8
Ignacio <ignacio.programmer@gmail.com>
2020-04-21 00:05:44 +0200

    Sound events.

---
+++ Assets/Scripts/Actor.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    private AudioSource currentAudioPlaceSelected;
+
+    public void SelectAudioPlace(AudioSource audioPlace)
+    {
+        currentAudioPlaceSelected = audioPlace;
+    }
+
+    public void PlayDialogue(ActorDialogue audioClip)
+    {
+        if(currentAudioPlaceSelected)
+        {
+            currentAudioPlaceSelected.Stop();
+            currentAudioPlaceSelected.clip = audioClip.dialogue;
+            currentAudioPlaceSelected.Play();
+        }
+    }
+}
---
+++ Assets/Scripts/ActorDialogue.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActorDialogue : MonoBehaviour
+{
+    public AudioClip dialogue;
+}
